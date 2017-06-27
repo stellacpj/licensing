c170dfa2f8e52e79a741e0e2559ffc4324e7c032
Stella Chen <stella@juniper.net>
2017-06-27 03:21:01 -0700

    add licensing disclaimer

--- models/menu.py
+++ models/menu.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
+# <*******************
+# 
+# Copyright 2017 Juniper Networks, Inc. All rights reserved.
+# Licensed under the Juniper Networks Script Software License (the "License").
+# You may not use this script file except in compliance with the License, which is located at
+# http://www.juniper.net/support/legal/scriptlicense/
+# Unless required by applicable law or otherwise agreed to in writing by the parties, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# 
+# *******************>
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Customize your APP title, subtitle and menus here
--- script/setup-web2py-ubuntu.sh
+++ script/setup-web2py-ubuntu.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# <*******************
+# 
+# Copyright 2017 Juniper Networks, Inc. All rights reserved.
+# Licensed under the Juniper Networks Script Software License (the "License").
+# You may not use this script file except in compliance with the License, which is located at
+# http://www.juniper.net/support/legal/scriptlicense/
+# Unless required by applicable law or otherwise agreed to in writing by the parties, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# 
+# *******************>
 echo "This script will:
 1) install all modules need to run web2py on Ubuntu 14.04
 2) install web2py in /home/www-data/
